39ebe868a27d60e4637154305e168110759af2f0
mengyue <m15032620398@163.com>
2020-09-17 17:40:53 +0800

    2020-09-17

--- src/views/coupon/EditCoupon.vue
+++ src/views/coupon/EditCoupon.vue
@@ -1,70 +1,71 @@
 <template>
-    <div class="gc-1">
-        <div class="h-1">
-            <div class="h-1-1">
-                更新优惠券
-            </div>
-            <Button type="primary" icon="ios-undo" @click="back">返回</Button>
-        </div>
-        <Form :model="couponData" label-position="left" :label-width="100">
-            <FormItem label="标题">
-                <Input v-model="couponData.title" placeholder="请输入标题" class="form"/>
-            </FormItem>
-            <FormItem label="满减额">
-                <InputNumber
-                        class="in-1"
-                        :max="10000"
-                        v-model="couponData.full_money"
-                        :formatter="value => `${parseFloat(value).toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                        :parser="value => value.replace(/\$\s?|(,*)/g, '')"
-                        :step="0.1">
-                </InputNumber>
-                <!--        <CountNumber v-model="skuData.price"></CountNumber>-->
-            </FormItem>
-            <FormItem label="优惠额">
-                <InputNumber
-                        class="in-1"
-                        :max="10000"
-                        v-model="couponData.minus"
-                        :formatter="value => `${parseFloat(value).toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                        :parser="value => value.replace(/\$\s?|(,*)/g, '')"
-                        :step="0.1">
-                </InputNumber>
-                <!--        <CountNumber v-model="skuData.price"></CountNumber>-->
-            </FormItem>
-            <FormItem label="折扣">
-                <InputNumber
-                        class="in-1"
-                        :max="1"
-                        v-model="couponData.rate"
-                        :formatter="value => `${parseFloat(value).toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                        :parser="value => value.replace(/\$\s?|(,*)/g, '')"
-                        :step="0.1">
-                </InputNumber>
-                <!--        <CountNumber v-model="skuData.price"></CountNumber>-->
-            </FormItem>
-            <FormItem label="类型">
-                <i-select v-model="couponData.type" clearable style="width:200px" placeholder="请选择类型">
-                    <i-option v-for="item in couponTypeList" :value="item.id" :key="item.id">{{ item.id+'-'+item.value
-                        }}
-                    </i-option>
-                </i-select>
-            </FormItem>
-            <FormItem label="时间">
-                <DatePicker @on-change="onChangeDate" :options="onOptions" v-model="start_end_time" type="daterange"
-                            format="yyyy-MM-dd" placeholder="选择开始-结束时间" style="width: 200px"></DatePicker>
-            </FormItem>
-            <FormItem label="描述">
-                <Input v-model="couponData.description" placeholder="请输入描述" class="form"/>
-            </FormItem>
-            <div class="gridCategoryBut">
-                <Button type="primary" class="saveGridCategoryBut"
-                        @click="updateSku(couponData)">保存
-                </Button>
-                <Button html-type="reset">重置</Button>
-            </div>
-        </Form>
+  <div class="gc-1">
+    <div class="h-1">
+      <div class="h-1-1">
+        更新优惠券
+      </div>
+      <Button type="primary" icon="ios-undo" @click="back">返回</Button>
     </div>
+    <Form :model="couponData" label-position="left" :label-width="100">
+      <FormItem label="标题">
+        <Input v-model="couponData.title" placeholder="请输入标题" class="form"/>
+      </FormItem>
+      <FormItem label="满减额">
+        <InputNumber
+                class="in-1"
+                :max="10000"
+                :min="0"
+                v-model="couponData.full_money"
+                :formatter="value => `${parseFloat(value).toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+                :parser="value => value.replace(/\$\s?|(,*)/g, '')"
+                :step="0.1">
+        </InputNumber>
+        <!--        <CountNumber v-model="skuData.price"></CountNumber>-->
+      </FormItem>
+      <FormItem label="优惠额">
+        <InputNumber
+                class="in-1"
+                :max="10000"
+                v-model="couponData.minus"
+                :formatter="value => `${parseFloat(value).toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+                :parser="value => value.replace(/\$\s?|(,*)/g, '')"
+                :step="0.1">
+        </InputNumber>
+        <!--        <CountNumber v-model="skuData.price"></CountNumber>-->
+      </FormItem>
+      <FormItem label="折扣">
+        <InputNumber
+                class="in-1"
+                :max="1"
+                v-model="couponData.rate"
+                :formatter="value => `${parseFloat(value).toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+                :parser="value => value.replace(/\$\s?|(,*)/g, '')"
+                :step="0.1">
+        </InputNumber>
+        <!--        <CountNumber v-model="skuData.price"></CountNumber>-->
+      </FormItem>
+      <FormItem label="类型">
+        <i-select v-model="couponData.type" clearable style="width:200px" placeholder="请选择类型">
+          <i-option v-for="item in couponTypeList" :value="item.id" :key="item.id">{{ item.id+'-'+item.value
+            }}
+          </i-option>
+        </i-select>
+      </FormItem>
+      <FormItem label="时间">
+        <DatePicker @on-change="onChangeDate" :options="onOptions" v-model="start_end_time" type="daterange"
+                    format="yyyy-MM-dd" placeholder="选择开始-结束时间" style="width: 200px"></DatePicker>
+      </FormItem>
+      <FormItem label="描述">
+        <Input v-model="couponData.description" placeholder="请输入描述" class="form"/>
+      </FormItem>
+      <div class="gridCategoryBut">
+        <Button type="primary" class="saveGridCategoryBut"
+                @click="updateSku(couponData)">保存
+        </Button>
+        <Button html-type="reset">重置</Button>
+      </div>
+    </Form>
+  </div>
 </template>
 
 <script>
@@ -91,27 +92,32 @@
                         value: '满金额折扣券'
                     }
                 ],
-                onOptions:{
-                    disabledDate(date){
+                onOptions: {
+                    disabledDate(date) {
                         return date && date.valueOf() < Date.now() - 86400000;
                     }
                 },
-                start_end_time:[]
+                start_end_time: []
             }
         },
         methods: {
-            initData(){
-              this.couponData = this.$route.query.data;
+            initData() {
+                this.couponData = this.$route.query.data;
+                this.couponData.full_money = this.couponData.full_money ? this.couponData.full_money : 0
+                this.couponData.minus = this.couponData.minus ? this.couponData.minus : 0
+                this.couponData.rate =this.couponData.rate?this.couponData.rate:0
             },
             back() {
                 this.$router.push('/couponList')
             },
-            onChangeDate(){
+            onChangeDate() {
 
             },
-            updateSku(){
+            updateSku() {
             }
         },
+        computed: {},
+        watch: {},
         created() {
             this.initData()
         }
@@ -119,49 +125,49 @@
 </script>
 
 <style scoped>
-    .gc-1 {
-        margin-right: 20px;
-        margin-left: 20px;
-    }
+  .gc-1 {
+    margin-right: 20px;
+    margin-left: 20px;
+  }
 
-    .h-1 {
+  .h-1 {
 
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        justify-items: center;
-        padding-bottom: 20px;
-    }
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    justify-items: center;
+    padding-bottom: 20px;
+  }
 
-    .h-1-1 {
-        color: #6739ff;
-    }
+  .h-1-1 {
+    color: #6739ff;
+  }
 
-    .ep-1 {
-        height: 22px;
-        width: 100px;
-        border-radius: 4px;
-    }
+  .ep-1 {
+    height: 22px;
+    width: 100px;
+    border-radius: 4px;
+  }
 
-    .gridCategoryBut {
-        padding-left: 100px;
-    }
+  .gridCategoryBut {
+    padding-left: 100px;
+  }
 
-    .saveGridCategoryBut {
-        margin-right: 20px;
-    }
+  .saveGridCategoryBut {
+    margin-right: 20px;
+  }
 
-    .form {
-        width: 550px;
-    }
+  .form {
+    width: 550px;
+  }
 
-    .in-1 {
-        width: 100px;
-    }
+  .in-1 {
+    width: 100px;
+  }
 
-    .dp-0 {
-        display: flex;
-        flex-direction: column;
-    }
+  .dp-0 {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
